291eb0b9245fe58b47be471a958bd5bf655d051d
Rednasrot <Rednasrot@gmail.com>
2025-01-03 16:40:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lekse-for-undervisning-18-03.01.2025
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Rednasrot/Lekse-for-undervisning-18-03.01.2025)
---
+++ script.js
@@ -0,0 +1,46 @@
+const carPush = document.querySelector('#carPush');
+const carPop = document.querySelector('#carPop');
+const carUnshift = document.querySelector('#carUnshift');
+const Shift = document.querySelector('#carShift');
+
+const carDisplay = [];
+
+carPush.addEventListener('click', function () {
+  let inputCar = document.querySelector('#inputCar');
+  carDisplay.push(inputCar.value);
+
+  let carDisplay = document.querySelector('#carDisplay');
+  carDisplay.textContent = carDisplay;
+
+  inputCar.value = '';
+});
+
+carPop.addEventListener('click', function () {
+  let inputCar = document.querySelector('#inputCar');
+  carDisplay.pop(inputCar.value);
+
+  let carDisplay = document.querySelector('#carDisplay');
+  carDisplay.textContent = carDisplay;
+
+  inputCar.value = '';
+});
+
+carUnshift.addEventListener('click', function () {
+  let inputCar = document.querySelector('#inputCar');
+  parkingLot.pop(inputCar.value);
+
+  let carDisplay = document.querySelector('#carDisplay');
+  carDisplay.textContent = carDisplay;
+
+  inputCar.value = '';
+});
+
+carShift.addEventListener('click', function () {
+  let inputCar = document.querySelector('#inputCar');
+  carDisplay.pop(inputCar.value);
+
+  let parkingLot = document.querySelector('#carDisplay');
+  carDisplay.textContent = parkingLot;
+
+  inputCar.value = '';
+});
